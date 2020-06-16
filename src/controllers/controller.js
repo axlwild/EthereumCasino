f93bdc5562a82e64f4517fd96fac387d5a2fc17b
Axel <axel_cg4@hotmail.com>
2020-06-16 14:32:52 -0500

    versión finall

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -11,10 +11,22 @@ exports.renderCasinoTragamonedas = (req, res) => {
     res.render('tragamonedas')
 }
 
+exports.renderCasinoTragamonedasV2 = (req, res) => {
+    res.render('tragamonedas_sp')
+}
+
 exports.renderNewKeno = (req, res) => {
     res.render('keno')
 }
+// Keno sin oraculo
+exports.renderNewKenoV2 = (req, res) => {
+    res.render('keno_v2')
+}
 exports.renderNewRuleta = (req, res) => {
     res.render('ruleta_new')
+}
+
+exports.renderNewRuletaV2 = (req, res) => {
+    res.render('ruleta_new_sin_oraculo')
 }
 
--- src/router.js
+++ src/router.js
@@ -1,9 +1,7 @@
 express = require('express')
 const router =  express.Router();
 const controller = require('./controllers/controller')
-router.get('/', function(req, res) {
-    res.send('Hola Mundo!');
-});
+router.get('/', controller.renderCasinoTragamonedas);
 
 router.get('/contract', controller.renderCasinoIndex );
 
@@ -14,6 +12,12 @@ router.get('/tragamonedas', controller.renderCasinoTragamonedas);
 router.get('/keno', controller.renderNewKeno);
 
 router.get('/ruleta', controller.renderNewRuleta);
+
+router.get('/ruletasn', controller.renderNewRuletaV2);
+
+router.get('/kenosn', controller.renderNewKenoV2);
+
+router.get('/tragamonedassn', controller.renderCasinoTragamonedasV2);
 
 
 module.exports = router
